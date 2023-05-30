0dba70cd6685de7c1ce67180894c785f6d1e878d
DCardigo <123099651+DCardigo@users.noreply.github.com>
2023-05-30 15:02:16 +0000

    Enlaces a variables, establecido

--- src/js/app.js
+++ src/js/app.js
@@ -29,15 +29,20 @@ function render(variables = {}) {
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
+  if (variables.name === null) variables.name = "Name, ";
+  if (variables.lastname === null) variables.lastname = "Lastname ";
+
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name}</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
+          
+          <h1> ${variables.name} ${variables.lastname}</h1>
+          
+          <h2>${variables.role}</h2>
+          <h3>${variables.city}, ${variables.country}</h3>
+          <ul class="${variables.socialMediaPosition}" >
+            <li><a href="https://twitter.com/4geeksacademy"><i class="ab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
             <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
